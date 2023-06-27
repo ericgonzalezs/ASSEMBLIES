db5eee350a19543cc35bb7d8156eece7a99da9a1
ericgonzalezs <shinken123@gmail.com>
2023-06-27 14:29:01 -0700

    Create 3D-DNA.sh

---
+++ 3D-DNA.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/aidenlab/3d-dna.git
+
+#create python env
+module load StdEnv/2020 python/3.11.2
+ virtualenv 3ddna
+
+  pip install scipy numpy matplotlib #libraries required for 3d-dna 
